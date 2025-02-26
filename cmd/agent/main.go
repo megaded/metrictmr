6b163d6ed72924befd88db3d8a7062905fc9478e
Евгений <>
2025-02-27 00:04:24 +0300

    remove stdout

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -16,21 +16,17 @@ func main() {
 	count := 0
 	for {
 		if count%pollInterval == 0 {
-			fmt.Println("Получение метрики", time.Now())
 			metrics = metricCollector.GetRunTimeMetrics()
 		}
 		if count%reportInterval == 0 {
 			for _, m := range metrics.GaugeMetrics {
 				url := fmt.Sprintf("/update/gauge/%s/%f", m.Name, m.Value)
-				fmt.Println(url)
 				resp, err := http.Post("http://localhost:8080"+url, "Content-Type: text/plain", nil)
 				if err != nil {
 					fmt.Println(err)
 				}
 				if resp.StatusCode != http.StatusOK {
-					fmt.Println("Ошибка ", url)
 				}
-				fmt.Println("Успех ", url)
 				resp.Body.Close()
 			}
 		}
