f6006826d6a44cc6d8eb15df490b9a2f38abebe6
Евгений <>
2025-02-28 13:43:43 +0300

    try fix agent

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -21,14 +21,7 @@ func main() {
 		}
 		if count%reportInterval == 0 {
 			for _, m := range metrics.GaugeMetrics {
-				url := fmt.Sprintf("/update/gauge/%s/%f", m.Name, m.Value)
-				fmt.Println(host + url)
-				resp, err := http.Post(host+url, "text/plain", http.NoBody)
-				if err != nil {
-					fmt.Println(err)
-				}
-				resp.Body.Close()
-
+				sendGaugeMetrics(string(m.Name), m.Value)
 			}
 			for _, m := range metrics.CounterMetrics {
 				url := fmt.Sprintf("/update/counter/%s/%d", m.Name, m.Value)
@@ -43,4 +36,23 @@ func main() {
 		time.Sleep(time.Second)
 		count++
 	}
+}
+
+func sendGaugeMetrics(metricName string, value float64) {
+	url := fmt.Sprintf("http://localhost:8080/update/gauge/%s/%f", metricName, value)
+	req, err := http.NewRequest(http.MethodPost, url, nil)
+	req.Header.Set("Content-type", "text-plain")
+	if err != nil {
+		fmt.Println("Error of creating request:", err)
+		return
+	}
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Error to sending request:", err)
+		return
+	}
+
+	defer resp.Body.Close()
 }
