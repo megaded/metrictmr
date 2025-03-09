c2f608acd37dcf39fb3894de12c1398a6dadf60a
Евгений <>
2025-03-10 00:12:17 +0300

    fix

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,22 +1,10 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/megaded/metrictmr/internal/agent"
 )
 
 func main() {
 	a := agent.CreateAgent()
 	a.StartSend()
-	http.HandleFunc()
-	f := func(http.ResponseWriter, *http.Request){}
-	http.Handle("1", f)
-
-
-	
-
-
-		
-	
 }
