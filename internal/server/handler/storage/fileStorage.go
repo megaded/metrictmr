48fdde031eca63b8952204c3f84636691a44f870
Евгений <>
2025-03-30 02:35:09 +0300

    fix

--- internal/server/handler/storage/fileStorage.go
+++ internal/server/handler/storage/fileStorage.go
@@ -21,7 +21,7 @@ func (s *FileStorage) GetGauge(name string) (metric data.Metric, exist bool) {
 	return s.m.GetGauge(name)
 }
 func (s *FileStorage) Store(metric ...data.Metric) {
-	s.m.Store(metric)
+	s.m.Store(metric...)
 	if s.internal == 0 {
 		s.persistData()
 	}
